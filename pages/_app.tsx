61b51d6c2ad9fb7e0e08bc4092afea0105c66484
sage <laymonage@gmail.com>
2021-03-18 22:21:43 +0700

    Use Tailwind CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from 'next/app';
 
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+};

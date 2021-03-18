792b259a64af80f5ad78eb8dddaa6d3944e915c0
sage <laymonage@gmail.com>
2021-03-18 21:35:11 +0700

    Use Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: 'John Doe' });
 };

e7fe52e1e5801ed5549559eb6e7b39c2b62b7000
sage <laymonage@gmail.com>
2021-03-19 13:31:33 +0700

    Adjust styles to be more flexible and accurate

--- components/Giscussions.tsx
+++ components/Giscussions.tsx
@@ -6,15 +6,15 @@ export default function Giscussions() {
       <div className="flex flex-wrap items-center">
         <h4 className="flex-auto my-2 mr-2 font-semibold">2 comments</h4>
       </div>
-      <div className="flex p-4">
+      <div className="flex p-4 text-sm">
         <div className="flex-shrink-0 mr-2 w-14">
           <div className="flex flex-col">
             <button type="button">
               <ArrowUpIcon />
             </button>
             <div className="flex justify-center w-full">
-              <div className="flex flex-row justify-center min-w-[26px] px-2 text-blue-900 bg-blue-100 rounded-full">
-                <div className="overflow-hidden text-sm">21</div>
+              <div className="flex flex-row justify-center min-w-[26px] px-2 py-1 text-blue-900 bg-blue-400 bg-opacity-20 rounded-full">
+                <div className="overflow-hidden text-xs">21</div>
               </div>
             </div>
             <button type="button">
@@ -22,7 +22,7 @@ export default function Giscussions() {
             </button>
           </div>
         </div>
-        <div className="w-full border border-blue-100 rounded-md">
+        <div className="w-full border border-blue-400 rounded-md border-opacity-30">
           <div className="flex items-center px-4">
             <h3 className="flex items-center flex-auto pt-2">
               <a href="https://github.com/laymonage" className="flex items-center">
@@ -33,7 +33,7 @@ export default function Giscussions() {
                   height="30"
                   alt="@laymonage"
                 />
-                <span className="text-bold">laymonage</span>
+                <span className="font-semibold">laymonage</span>
               </a>
               <a
                 href="#discussioncomment-123"
@@ -49,8 +49,12 @@ export default function Giscussions() {
                 </div>
               </a>
               <div className="hidden ml-2 text-xs sm:inline-flex">
-                <span className="px-1 ml-1 border border-blue-100 rounded-md">Maintainer</span>
-                <span className="px-1 ml-1 border border-blue-100 rounded-md">Author</span>
+                <span className="px-1 ml-1 border border-blue-400 rounded-md border-opacity-30">
+                  Maintainer
+                </span>
+                <span className="px-1 ml-1 border border-blue-400 rounded-md border-opacity-30">
+                  Author
+                </span>
               </div>
             </h3>
             <div className="flex">
@@ -63,7 +67,7 @@ export default function Giscussions() {
             <div className="w-full p-4">Test comment.</div>
             <div className="flex content-center justify-between">
               <div className="ml-4">
-                <button className="px-2 py-1 mr-2 text-sm border rounded">
+                <button className="px-2 py-1 mb-4 mr-2 border rounded">
                   <img
                     className="inline-block mr-1"
                     alt="eyes"
@@ -75,11 +79,11 @@ export default function Giscussions() {
                 </button>
               </div>
               <div className="mb-4 mr-4">
-                <span className="text-sm text-gray-500">2 replies</span>
+                <span className="text-xs text-gray-500">2 replies</span>
               </div>
             </div>
           </div>
-          <div className="pt-2 bg-gray-50">
+          <div className="pt-2 bg-blue-900 bg-opacity-5">
             <div className="flex pt-2 pl-4">
               <div className="w-[2px] bg-gray-200 relative left-4 z-0"></div>
               <div className="z-10">
@@ -93,11 +97,11 @@ export default function Giscussions() {
                   />
                 </a>
               </div>
-              <div className="w-full mb-6 ml-2">
+              <div className="w-full mb-2 ml-2">
                 <div className="flex">
                   <h3 className="flex items-center flex-auto pt-1">
                     <a href="https://github.com/laymonage" className="flex items-center">
-                      <span className="text-bold">laymonage</span>
+                      <span className="font-semibold">laymonage</span>
                     </a>
                     <a
                       href="#discussioncomment-123"
@@ -113,10 +117,12 @@ export default function Giscussions() {
                       </div>
                     </a>
                     <div className="hidden ml-2 text-xs sm:inline-flex">
-                      <span className="px-1 ml-1 border border-blue-100 rounded-md">
+                      <span className="px-1 ml-1 border border-blue-400 rounded-md border-opacity-30">
                         Maintainer
                       </span>
-                      <span className="px-1 ml-1 border border-blue-100 rounded-md">Author</span>
+                      <span className="px-1 ml-1 border border-blue-400 rounded-md border-opacity-30">
+                        Author
+                      </span>
                     </div>
                   </h3>
                   <div className="flex mr-4">
@@ -129,7 +135,7 @@ export default function Giscussions() {
                   <div className="w-full py-4">Test reply.</div>
                   <div className="flex content-center justify-between">
                     <div className="">
-                      <button className="px-2 py-1 mr-2 text-sm border rounded">
+                      <button className="px-2 py-1 mb-4 mr-2 border rounded">
                         <img
                           className="inline-block mr-1"
                           alt="eyes"
@@ -154,7 +160,7 @@ export default function Giscussions() {
                   alt="@laymonage"
                 />
               </a>
-              <button className="w-full px-2 py-1 ml-2 text-sm text-left text-gray-600 bg-white border rounded">
+              <button className="w-full px-2 py-1 ml-2 text-left text-gray-600 bg-white border rounded">
                 Write a reply
               </button>
             </div>

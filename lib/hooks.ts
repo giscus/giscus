d8c03106d86c252ac9abb68ac562dbc1ae8192f3
sage <laymonage@gmail.com>
2021-03-20 18:24:47 +0700

    Create useComponentVisible hook

---
+++ lib/hooks.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef, MutableRefObject, Dispatch, SetStateAction } from 'react';
+
+export default function useComponentVisible<T extends HTMLElement>(initialIsVisible: boolean) {
+  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
+  const ref = useRef(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
+
+  return [ref, isComponentVisible, setIsComponentVisible] as [
+    MutableRefObject<T>,
+    boolean,
+    Dispatch<SetStateAction<boolean>>,
+  ];
+}

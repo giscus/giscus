d81904cb6d604b1ad1d99c6eafc0d1dfcb03ffec
sage <laymonage@gmail.com>
2021-03-21 08:59:44 +0700

    Create Reactions static object

---
+++ lib/reactions.ts
@@ -0,0 +1,12 @@
+export const Reactions = {
+  THUMBS_UP: { name: '+1', emoji: '👍' },
+  THUMBS_DOWN: { name: '-1', emoji: '👎' },
+  LAUGH: { name: 'Laugh', emoji: '😆' },
+  HOORAY: { name: 'Hooray', emoji: '🎉' },
+  CONFUSED: { name: 'Confused', emoji: '😕' },
+  LOVE: { name: 'Love', emoji: '❤️' },
+  ROCKET: { name: 'Rocket', emoji: '🚀' },
+  EYES: { name: 'Eyes', emoji: '👀' },
+} as const;
+
+export type Reactions = keyof typeof Reactions;

40abcfb33ff27480cc53dd35c82ecc49e0241516
sage <laymonage@gmail.com>
2021-03-27 23:09:40 +0700

    Put variables in lib/variables

---
+++ lib/variables.ts
@@ -0,0 +1,4 @@
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+export const ENCRYPTION_PASSWORD = process.env.ENCRYPTION_PASSWORD;

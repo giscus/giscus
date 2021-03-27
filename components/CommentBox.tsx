af30b2a74bd9958bc4263a09714a2a4302a2fe98
sage <laymonage@gmail.com>
2021-03-27 09:45:33 +0700

    Implement markdown preview in CommentBox

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -1,8 +1,25 @@
 import { MarkdownIcon } from '@primer/octicons-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { renderMarkdown } from '../services/github/markdown';
 
 export default function CommentBox() {
   const [isPreview, setIsPreview] = useState(false);
+  const [input, setInput] = useState('');
+  const [lastInput, setLastInput] = useState('');
+  const [preview, setPreview] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (isPreview && input !== lastInput) {
+      setIsLoading(true);
+      renderMarkdown(input).then((value) => {
+        setPreview(value);
+        setIsLoading(false);
+      });
+      setLastInput(input);
+    }
+  }, [isPreview, input, lastInput]);
+
   return (
     <div className="text-sm border rounded">
       <div className="flex bg-gray-500 border-b rounded-t bg-opacity-5">
@@ -13,7 +30,7 @@ export default function CommentBox() {
                 ? 'text-gray-700 bg-white border border-b-0 rounded-t'
                 : 'text-gray-500 border-transparent'
             }`}
-            onClick={() => setIsPreview(false)}
+            onClick={() => setIsPreview(!isPreview)}
           >
             Write
           </button>
@@ -21,7 +38,7 @@ export default function CommentBox() {
             className={`px-4 py-2 border border-b-0 focus:outline-none ml-1 ${
               isPreview ? 'text-gray-700 bg-white rounded-t' : 'text-gray-500 border-transparent'
             }`}
-            onClick={() => setIsPreview(true)}
+            onClick={() => setIsPreview(!isPreview)}
           >
             Preview
           </button>
@@ -29,11 +46,18 @@ export default function CommentBox() {
       </div>
       <div className="m-2">
         {isPreview ? (
-          <div className="px-2 pt-2 pb-4 min-h-[105px] border-b-2">Nothing to preview</div>
+          <div
+            className="px-2 pt-2 pb-4 min-h-[105px] border-b-2 markdown"
+            dangerouslySetInnerHTML={isLoading ? undefined : { __html: preview }}
+          >
+            {isLoading ? 'Loading preview...' : undefined}
+          </div>
         ) : (
           <textarea
             className="w-full p-2 border rounded min-h-[100px]"
             placeholder="Write a comment"
+            onChange={(event) => setInput(event.target.value)}
+            value={input}
           ></textarea>
         )}
       </div>

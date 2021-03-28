9b26412412942d1ebd67236aaaf46a05fa150ea8
sage <laymonage@gmail.com>
2021-03-28 21:37:09 +0700

    Add workaround to get readonly access token

--- lib/variables.ts
+++ lib/variables.ts
@@ -2,6 +2,7 @@ export const env = {
   app_id: process.env.GITHUB_APP_ID,
   client_id: process.env.GITHUB_CLIENT_ID,
   client_secret: process.env.GITHUB_CLIENT_SECRET,
+  installation_id: process.env.GITHUB_INSTALLATION_ID,
   token: process.env.GITHUB_TOKEN,
   private_key: process.env.GITHUB_PRIVATE_KEY,
   encryption_password: process.env.ENCRYPTION_PASSWORD,

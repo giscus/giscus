7cc7124cab2359df7fcca5d360952dd14722f8bf
sage <laymonage@gmail.com>
2021-04-02 08:39:13 +0700

    Remove default Next.js home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,26 @@
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import Giscussions from '../components/Giscussions';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const route = useRouter();
   const session = route.query.session as string;
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Giscussions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-
-        <div className="w-full max-w-3xl">
-          <Giscussions
-            repositoryOwner="laymonage"
-            repositoryName="discussions-playground"
-            discussionNumber="4"
-            session={session}
-          />
-        </div>
+      <main className="w-full max-w-3xl mx-auto my-4">
+        <Giscussions
+          repositoryOwner="laymonage"
+          repositoryName="discussions-playground"
+          discussionNumber="4"
+          session={session}
+        />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }

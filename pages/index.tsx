574f8cea0d0332a669cc07f10d83fbb0a8e553ad
sage <laymonage@gmail.com>
2021-04-02 18:46:51 +0700

    Add origin to AuthContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,17 @@ export default function Home() {
   const route = useRouter();
   const session = route.query.session as string;
   const [token, setToken] = useState('');
+  const [origin, setOrigin] = useState('');
 
   useEffect(() => {
     if (session) {
       getToken(session).then(setToken);
     }
   }, [session]);
+
+  useEffect(() => {
+    setOrigin((route.query.origin as string) || location.href || '');
+  }, [origin, route.query.origin]);
 
   return (
     <>
@@ -24,7 +29,7 @@ export default function Home() {
       </Head>
 
       <main className="w-full max-w-3xl mx-auto my-4">
-        <AuthContext.Provider value={{ token }}>
+        <AuthContext.Provider value={{ token, origin }}>
           <Giscussions
             repositoryOwner="laymonage"
             repositoryName="discussions-playground"

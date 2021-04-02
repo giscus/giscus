de1b104c21dec266720bea3a6fbc5fc758faf071
sage <laymonage@gmail.com>
2021-04-02 17:31:28 +0700

    Require sign in to use the comment box

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -19,8 +19,6 @@ export default function CommentBox() {
           setPreview(value);
           setIsLoading(false);
         });
-      } else {
-        setPreview('Nothing to preview');
       }
       setLastInput(input);
     }
@@ -36,7 +34,7 @@ export default function CommentBox() {
                 ? 'text-gray-700 bg-white border border-b-0 rounded-t'
                 : 'text-gray-500 border-transparent'
             }`}
-            onClick={() => setIsPreview(!isPreview)}
+            onClick={() => setIsPreview(false)}
           >
             Write
           </button>
@@ -44,7 +42,7 @@ export default function CommentBox() {
             className={`px-4 py-2 border border-b-0 focus:outline-none ml-1 ${
               isPreview ? 'text-gray-700 bg-white rounded-t' : 'text-gray-500 border-transparent'
             }`}
-            onClick={() => setIsPreview(!isPreview)}
+            onClick={() => setIsPreview(true)}
           >
             Preview
           </button>
@@ -54,16 +52,19 @@ export default function CommentBox() {
         {isPreview ? (
           <div
             className="px-2 pt-2 pb-4 min-h-[105px] border-b-2 markdown"
-            dangerouslySetInnerHTML={isLoading ? undefined : { __html: preview }}
+            dangerouslySetInnerHTML={
+              isLoading ? undefined : { __html: preview || 'Nothing to preview' }
+            }
           >
             {isLoading ? 'Loading preview...' : undefined}
           </div>
         ) : (
           <textarea
             className="w-full p-2 border rounded min-h-[100px]"
-            placeholder="Write a comment"
+            placeholder={token ? 'Write a comment' : 'Sign in to comment'}
             onChange={(event) => setInput(event.target.value)}
             value={input}
+            disabled={!token}
           ></textarea>
         )}
       </div>
@@ -77,8 +78,27 @@ export default function CommentBox() {
           <MarkdownIcon className="mr-1" />
           Styling with Markdown is supported
         </a>
-        <button className="px-4 py-[5px] ml-1 text-white bg-[#2ea44f] hover:bg-[#2c974b] border-[rgba(27,31,35,0.15)]  rounded-md">
-          Comment
+        <button className="px-4 py-[5px] ml-1 text-white bg-[#2ea44f] hover:bg-[#2c974b] border-[rgba(27,31,35,0.15)] rounded-md inline-flex items-center">
+          {token ? (
+            `Comment`
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 11.493 11.209"
+                className="mr-2"
+              >
+                <path
+                  fill="#fff"
+                  fillRule="evenodd"
+                  d="M5.746 0A5.747 5.747 0 003.93 11.2c.287.052.392-.125.392-.278 0-.136-.005-.497-.008-.977-1.598.347-1.935-.77-1.935-.77-.262-.664-.639-.841-.639-.841-.521-.356.04-.35.04-.35.577.041.88.593.88.593.513.878 1.345.625 1.673.477.052-.37.2-.624.364-.768-1.276-.145-2.617-.638-2.617-2.84 0-.627.224-1.14.591-1.542-.059-.145-.256-.73.057-1.52 0 0 .482-.155 1.58.589.458-.128.95-.192 1.438-.194.489.002.98.066 1.44.194 1.096-.744 1.578-.59 1.578-.59.313.791.116 1.376.057 1.521.369.402.59.915.59 1.542 0 2.208-1.343 2.694-2.623 2.836.206.177.39.528.39 1.064 0 .768-.007 1.388-.007 1.576 0 .154.104.333.395.277A5.749 5.749 0 005.746 0"
+                />
+              </svg>{' '}
+              Sign in with GitHub
+            </>
+          )}
         </button>
       </div>
     </div>

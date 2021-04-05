1f25c18be00397f7a5c6092b9a2a1d8187529e36
sage <laymonage@gmail.com>
2021-04-06 04:02:16 +0700

    Create addDiscussionComment service function

---
+++ services/github/addDiscussionComment.ts
@@ -0,0 +1,80 @@
+import { GComment } from '../../lib/models/github';
+
+const ADD_DISCUSSION_COMMENT_QUERY = `
+  mutation($body: String!, $discussionId: ID!) {
+    addDiscussionComment(input: {body: $body, discussionId: $discussionId}) {
+      comment {
+        upvoteCount
+        viewerHasUpvoted
+        author {
+          avatarUrl
+          login
+          url
+        }
+        createdAt
+        url
+        authorAssociation
+        includesCreatedEdit
+        bodyHTML
+        reactionGroups {
+          content
+          users {
+            totalCount
+          }
+          viewerHasReacted
+        }
+        replies(first: 100) {
+          totalCount
+          nodes {
+            author {
+              avatarUrl
+              login
+              url
+            }
+            createdAt
+            url
+            authorAssociation
+            includesCreatedEdit
+            bodyHTML
+            reactionGroups {
+              content
+              users {
+                totalCount
+              }
+              viewerHasReacted
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+export interface AddDiscussionCommentBody {
+  body: string;
+  discussionId: string;
+}
+
+export interface AddDiscussionCommentResponse {
+  data: {
+    addDiscussionComment: {
+      comment: GComment;
+    };
+  };
+}
+
+export async function addDiscussionComment(
+  params: AddDiscussionCommentBody,
+  token: string,
+): Promise<AddDiscussionCommentResponse> {
+  return fetch('/api/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: ADD_DISCUSSION_COMMENT_QUERY,
+      variables: params,
+    }),
+  }).then((r) => r.json());
+}

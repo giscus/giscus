3b7e65a7ca81043bbd6aa1e92744b774cefb3303
sage <laymonage@gmail.com>
2021-04-07 05:52:05 +0700

    Revert "Use Tailwind's built-in jit mode"
    This reverts commit 6f1d0e762a110e8171eac92fefff4756e0269a2a.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {

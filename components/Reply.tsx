d88566ba8deeaf67f11615cd19f90fb8431b5a16
sage <laymonage@gmail.com>
2021-04-08 09:33:35 +0700

    Update Reply UI to match GitHub update

--- components/Reply.tsx
+++ components/Reply.tsx
@@ -9,66 +9,65 @@ export interface IReplyProps {
 
 export default function Reply({ reply }: IReplyProps) {
   return (
-    <>
-      <div className="relative pt-2 bg-gray-500 bg-opacity-5 gsc-reply">
-        <div className="w-[2px] flex-shrink-0 bg-gray-500 bg-opacity-10 absolute left-[30px] h-full top-0 gsc-tl-line">
-          <style jsx>
-            {`
-              :global(.gsc-reply):first-child > .gsc-tl-line {
-                top: 16px;
-                height: calc(100% - 16px);
-              }
-            `}
-          </style>
+    <div className="relative gsc-reply">
+      <div className="w-[2px] flex-shrink-0 bg-gray-500 bg-opacity-10 absolute left-[30px] h-full top-0 gsc-tl-line">
+        <style jsx>
+          {`
+            :global(.gsc-reply):first-child > .gsc-tl-line {
+              top: 16px;
+              height: calc(100% - 16px);
+            }
+          `}
+        </style>
+      </div>
+      <div className="flex py-2 pl-4">
+        <div className="z-10 flex-shrink-0">
+          <a href={reply.author.url} className="flex items-center">
+            <img
+              className="rounded-full"
+              src={reply.author.avatarUrl}
+              width="30"
+              height="30"
+              alt={`@${reply.author.login}`}
+            />
+          </a>
         </div>
-        <div className="flex pt-2 pl-4">
-          <div className="z-10 flex-shrink-0">
-            <a href={reply.author.url} className="flex items-center">
-              <img
-                className="rounded-full"
-                src={reply.author.avatarUrl}
-                width="30"
-                height="30"
-                alt={`@${reply.author.login}`}
-              />
-            </a>
-          </div>
-          <div className="w-full min-w-0 ml-2">
-            <div className="flex">
-              <h3 className="flex items-center flex-auto pt-1">
-                <a href={reply.author.url} className="flex items-center">
-                  <span className="font-semibold">{reply.author.login}</span>
-                </a>
-                <a href={reply.url} className="ml-2 text-gray-500">
-                  <div
-                    className="whitespace-nowrap"
-                    title={format(new Date(reply.createdAt), 'LLL d, y, p O')}
-                  >
-                    {formatDistance(new Date(reply.createdAt), new Date(), { addSuffix: true })}
-                  </div>
-                </a>
-                <div className="hidden ml-2 text-xs sm:inline-flex">
-                  <span className="px-1 ml-1 capitalize border border-blue-400 rounded-md border-opacity-30">
-                    {reply.authorAssociation.toLowerCase()}
-                  </span>
+        <div className="w-full min-w-0 ml-2">
+          <div className="flex">
+            <h3 className="flex items-start flex-auto">
+              <a href={reply.author.url} className="flex items-center">
+                <span className="font-semibold">{reply.author.login}</span>
+              </a>
+              <a href={reply.url} className="ml-2 text-gray-500">
+                <div
+                  className="whitespace-nowrap"
+                  title={format(new Date(reply.createdAt), 'LLL d, y, p O')}
+                >
+                  {formatDistance(new Date(reply.createdAt), new Date(), { addSuffix: true })}
                 </div>
-              </h3>
-              <div className="flex pr-4">
-                <button className="text-gray-500 hover:text-blue-600">
-                  <KebabHorizontalIcon />
-                </button>
+              </a>
+              <div className="hidden ml-2 text-xs sm:inline-flex">
+                <span className="px-1 ml-1 capitalize border border-blue-400 rounded-md border-opacity-30">
+                  {reply.authorAssociation.toLowerCase()}
+                </span>
               </div>
+            </h3>
+            <div className="flex pr-4">
+              <ReactButtons reactionGroups={reply.reactions} variant="popoverOnly" />
+              <button className="text-gray-500 hover:text-blue-600">
+                <KebabHorizontalIcon />
+              </button>
             </div>
-            <div
-              className="w-full py-2 pr-4 markdown"
-              dangerouslySetInnerHTML={{ __html: reply.bodyHTML }}
-            ></div>
-            <div className="relative flex content-center mr-4">
-              <ReactButtons reactionGroups={reply.reactions} />
-            </div>
+          </div>
+          <div
+            className="w-full pb-2 pr-4 markdown"
+            dangerouslySetInnerHTML={{ __html: reply.bodyHTML }}
+          ></div>
+          <div className="relative flex content-center mr-4">
+            <ReactButtons reactionGroups={reply.reactions} variant="groupsOnly" />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }

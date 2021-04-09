7d1f9b92bce904c89f9b881b510bf74f660ecc43
sage <laymonage@gmail.com>
2021-04-09 17:17:13 +0700

    Create library functions for date formatting

---
+++ lib/string.ts
@@ -0,0 +1,9 @@
+import { format, formatDistanceStrict } from 'date-fns';
+
+export function formatDate(dt: string) {
+  return format(new Date(dt), 'LLL d, y, p O');
+}
+
+export function formatDateDistance(dt: string) {
+  return formatDistanceStrict(new Date(dt), new Date(), { addSuffix: true });
+}

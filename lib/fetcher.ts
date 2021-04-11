9ca1bad4e80b4df73f5fb0759c0340cada352391
sage <laymonage@gmail.com>
2021-04-11 11:44:37 +0700

    Use isNullish in param functions

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,5 @@
+import { isNullish } from './utils';
+
 export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
   const res = await fetch(input, init);
   return res.json();
@@ -5,11 +7,15 @@ export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
 
 export const cleanParams = (params: Record<string, unknown>) =>
   Object.entries(params).reduce(
-    (prev, [key, value]) => (value ? { ...prev, [key]: value } : prev),
+    (prev, [key, value]) => (!isNullish(value) ? { ...prev, [key]: value } : prev),
     {},
   );
 
 export const reduceParams = (params: Record<string, unknown>) =>
   Object.entries(params)
-    .reduce((prev, [key, value]) => (value ? `${prev} ${key}: ${JSON.stringify(value)}` : prev), '')
+    .reduce(
+      (prev, [key, value]) =>
+        !isNullish(value) ? `${prev} ${key}: ${JSON.stringify(value)}` : prev,
+      '',
+    )
     .trim();

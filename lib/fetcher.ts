aa9619912e5e00e09d0098e8b7b764b92368970f
sage <laymonage@gmail.com>
2021-04-11 07:10:26 +0700

    Create cleanParams and reduceParams functions

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -2,3 +2,14 @@ export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
   const res = await fetch(input, init);
   return res.json();
 };
+
+export const cleanParams = (params: Record<string, unknown>) =>
+  Object.entries(params).reduce(
+    (prev, [key, value]) => (value ? { ...prev, [key]: value } : prev),
+    {},
+  );
+
+export const reduceParams = (params: Record<string, unknown>) =>
+  Object.entries(params)
+    .reduce((prev, [key, value]) => (value ? `${prev} ${key}: ${JSON.stringify(value)}` : prev), '')
+    .trim();

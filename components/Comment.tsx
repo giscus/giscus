6859ae8d1415c8002172a44d0a6278599f7c78be
sage <laymonage@gmail.com>
2021-04-13 03:49:45 +0700

    Do not show hidden/deleted comments and replies

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -29,73 +29,92 @@ export default function Comment({
     [comment, onCommentUpdate],
   );
 
+  const hidden = comment.deletedAt || comment.isMinimized;
+
   return (
     <div className="flex my-4 text-sm">
-      <div className="flex-shrink-0 mr-2 w-14">
-        <div className="flex flex-col">
-          <button type="button" className={`${comment.viewerHasUpvoted ? 'text-blue-600' : ''}`}>
-            <ArrowUpIcon className="transform hover:translate-y-[-10%] transition-transform ease-in-out duration-150" />
-          </button>
-          <div className="flex justify-center w-full">
-            <div className="flex flex-row justify-center min-w-[26px] px-2 py-1 text-blue-900 bg-blue-400 bg-opacity-20 rounded-full">
-              <div className="overflow-hidden text-xs">{comment.upvoteCount}</div>
+      {!comment.isMinimized ? (
+        <div className="flex-shrink-0 mr-2 w-14">
+          <div className="flex flex-col">
+            <button type="button" className={`${comment.viewerHasUpvoted ? 'text-blue-600' : ''}`}>
+              <ArrowUpIcon className="transform hover:translate-y-[-10%] transition-transform ease-in-out duration-150" />
+            </button>
+            <div className="flex justify-center w-full">
+              <div className="flex flex-row justify-center min-w-[26px] px-2 py-1 text-blue-900 bg-blue-400 bg-opacity-20 rounded-full">
+                <div className="overflow-hidden text-xs">{comment.upvoteCount}</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : null}
       <div className="w-full min-w-0 border border-blue-400 rounded-md border-opacity-30">
-        <div className="flex items-center px-4">
-          <h3 className="flex items-center flex-auto pt-2">
-            <a href={comment.author.url} className="flex items-center">
-              <img
-                className="mr-2 rounded-full"
-                src={comment.author.avatarUrl}
-                width="30"
-                height="30"
-                alt={`@${comment.author.login}`}
-              />
-              <span className="font-semibold">{comment.author.login}</span>
-            </a>
-            <a href={comment.url} className="ml-2 text-gray-500">
-              <div className="whitespace-nowrap" title={formatDate(comment.createdAt)}>
-                {formatDateDistance(comment.createdAt)}
+        {!comment.isMinimized ? (
+          <div className="flex items-center px-4">
+            <h3 className="flex items-center flex-auto pt-2">
+              <a href={comment.author.url} className="flex items-center">
+                <img
+                  className="mr-2 rounded-full"
+                  src={comment.author.avatarUrl}
+                  width="30"
+                  height="30"
+                  alt={`@${comment.author.login}`}
+                />
+                <span className="font-semibold">{comment.author.login}</span>
+              </a>
+              <a href={comment.url} className="ml-2 text-gray-500">
+                <div className="whitespace-nowrap" title={formatDate(comment.createdAt)}>
+                  {formatDateDistance(comment.createdAt)}
+                </div>
+              </a>
+              <div className="hidden ml-2 text-xs sm:inline-flex">
+                <span className="px-1 ml-1 capitalize border border-blue-400 rounded-md border-opacity-30">
+                  {comment.authorAssociation}
+                </span>
               </div>
-            </a>
-            <div className="hidden ml-2 text-xs sm:inline-flex">
-              <span className="px-1 ml-1 capitalize border border-blue-400 rounded-md border-opacity-30">
-                {comment.authorAssociation}
+            </h3>
+            <div className="flex">
+              {comment.lastEditedAt ? (
+                <button
+                  className="hidden mr-2 text-gray-600 sm:inline-block"
+                  title={`Last edited at ${formatDate(comment.lastEditedAt)}`}
+                >
+                  edited
+                </button>
+              ) : null}
+              <button className="hidden text-gray-500 hover:text-blue-600 sm:inline-block">
+                <KebabHorizontalIcon />
+              </button>
+            </div>
+          </div>
+        ) : null}
+        <div
+          className={`markdown ${
+            comment.isMinimized ? 'px-4 py-2 bg-gray-500 bg-opacity-5' : 'p-4'
+          }`}
+          dangerouslySetInnerHTML={hidden ? undefined : { __html: comment.bodyHTML }}
+        >
+          <em className="text-gray-500">
+            This comment {comment.deletedAt ? 'was deleted' : 'has been minimized'}.
+          </em>
+        </div>
+        {!comment.isMinimized ? (
+          <div className="flex content-center justify-between">
+            <div className="flex mx-4">
+              {!hidden ? (
+                <ReactButtons
+                  reactionGroups={comment.reactions}
+                  subjectId={comment.id}
+                  onReact={updateReactions}
+                />
+              ) : null}
+            </div>
+            <div className="mb-4 mr-4">
+              <span className="text-xs text-gray-500">
+                {comment.replies.length}&nbsp;{comment.replies.length === 1 ? 'reply' : 'replies'}
               </span>
             </div>
-          </h3>
-          <div className="flex">
-            {comment.lastEditedAt ? (
-              <button
-                className="hidden mr-2 text-gray-600 sm:inline-block"
-                title={`Last edited at ${formatDate(comment.lastEditedAt)}`}
-              >
-                edited
-              </button>
-            ) : null}
-            <button className="hidden text-gray-500 hover:text-blue-600 sm:inline-block">
-              <KebabHorizontalIcon />
-            </button>
           </div>
-        </div>
-        <div className="p-4 markdown" dangerouslySetInnerHTML={{ __html: comment.bodyHTML }}></div>
-        <div className="flex content-center justify-between">
-          <div className="flex mx-4">
-            <ReactButtons
-              reactionGroups={comment.reactions}
-              subjectId={comment.id}
-              onReact={updateReactions}
-            />
-          </div>
-          <div className="mb-4 mr-4">
-            <span className="text-xs text-gray-500">
-              {comment.replies.length}&nbsp;{comment.replies.length === 1 ? 'reply' : 'replies'}
-            </span>
-          </div>
-        </div>
+        ) : null}
         {comment.replies.length > 0 ? (
           <div className="pt-2 bg-gray-500 border-t bg-opacity-5">
             {replies.map((reply) => (
@@ -111,7 +130,7 @@ export default function Comment({
             ) : null}
           </div>
         ) : null}
-        {children}
+        {!hidden ? children : null}
       </div>
     </div>
   );

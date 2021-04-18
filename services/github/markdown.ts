e46e9b792b34be5611e3a7359274960b15538f57
sage <laymonage@gmail.com>
2021-04-18 07:25:23 +0700

    Allow renderMarkdown to accept context

--- services/github/markdown.ts
+++ services/github/markdown.ts
@@ -1,9 +1,9 @@
 const GITHUB_API_URL = 'https://api.github.com/markdown';
 
-export async function renderMarkdown(text: string, token?: string) {
+export async function renderMarkdown(text: string, token?: string, context?: string) {
   return fetch(GITHUB_API_URL, {
     method: 'POST',
     headers: token ? { Authorization: `token ${token}` } : {},
-    body: JSON.stringify({ mode: 'gfm', text }),
+    body: JSON.stringify({ mode: 'gfm', text, ...(context ? { context } : {}) }),
   }).then((r) => r.text());
 }

bc96fea9e4b08c93a5d7e88a779ca6be87b8ece4
sage <laymonage@gmail.com>
2021-04-19 13:12:17 +0700

    Fix commit-tease-sha link

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -22,27 +22,45 @@ export function adaptAuthorAssociation(association: GCommentAuthorAssociation) {
   return association === 'NONE' ? '' : association.toLowerCase().replace('_', ' ');
 }
 
+export function adaptBodyHTML(bodyHTML: string) {
+  return bodyHTML.replace(
+    '<a data-pjax="true" class="commit-tease-sha" href="',
+    '<a data-pjax="true" class="commit-tease-sha" href="https://github.com',
+  );
+}
+
 export function adaptReply(reply: GReply): IReply {
   const {
     reactionGroups,
     replyTo: { id: replyToId },
     authorAssociation: association,
+    bodyHTML: body,
     ...rest
   } = reply;
+
   const authorAssociation = adaptAuthorAssociation(association);
   const reactions = adaptReactionGroups(reactionGroups);
-  return { ...rest, authorAssociation, reactions, replyToId };
+  const bodyHTML = adaptBodyHTML(body);
+
+  return { ...rest, bodyHTML, authorAssociation, reactions, replyToId };
 }
 
 export function adaptComment(comment: GComment): IComment {
-  const { replies: repliesData, reactionGroups, authorAssociation: association, ...rest } = comment;
+  const {
+    replies: repliesData,
+    reactionGroups,
+    authorAssociation: association,
+    bodyHTML: body,
+    ...rest
+  } = comment;
   const { totalCount: replyCount, nodes: replyNodes } = repliesData;
 
   const authorAssociation = adaptAuthorAssociation(association);
   const reactions = adaptReactionGroups(reactionGroups);
+  const bodyHTML = adaptBodyHTML(body);
   const replies = replyNodes.map(adaptReply);
 
-  return { ...rest, authorAssociation, replyCount, reactions, replies };
+  return { ...rest, bodyHTML, authorAssociation, replyCount, reactions, replies };
 }
 
 export function adaptDiscussion({

c7d0d6a300f64c7c5b8db97213fb9a7545dc72d5
sage <laymonage@gmail.com>
2021-04-25 06:13:54 +0700

    Add iframe-resizer to widget page

--- pages/widget.tsx
+++ pages/widget.tsx
@@ -28,6 +28,12 @@ export default function Home() {
           'Please provide repo and term query parameters.'
         ) : null}
       </main>
+
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.1/iframeResizer.contentWindow.min.js"
+        integrity="sha512-qw2bX9KUhi7HLuUloyRsvxRlWJvj0u0JWVegc5tf7qsw47T0pwXZIk1Kyc0utTH3NlrpHtLa4HYTVUyHBr9Ufg=="
+        crossOrigin="anonymous"
+      ></script>
     </>
   );
 }

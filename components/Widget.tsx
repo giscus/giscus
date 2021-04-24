305f98c7f33088f4bf150d0a6b986a4817c5c715
sage <laymonage@gmail.com>
2021-04-24 12:17:25 +0700

    Extract Widget component

---
+++ components/Widget.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import Giscussions from '../components/Giscussions';
+import { AuthContext } from '../lib/context';
+import { getToken } from '../services/giscussions/token';
+
+const GISCUSSIONS_SESSION_KEY = 'giscussions-session';
+
+export default function Widget({ repo, term }: { repo: string; term: string }) {
+  const router = useRouter();
+  const [token, setToken] = useState('');
+  const [origin, setOrigin] = useState('');
+
+  const querySession = router.query.giscussions as string;
+
+  let savedSession: string;
+  try {
+    savedSession = JSON.parse(localStorage.getItem(GISCUSSIONS_SESSION_KEY));
+  } catch (err) {
+    savedSession = '';
+  }
+
+  const session = querySession || savedSession;
+
+  if (session) getToken(session).then(setToken);
+
+  if (querySession) {
+    localStorage.setItem(GISCUSSIONS_SESSION_KEY, JSON.stringify(querySession));
+    router.replace(router.pathname, undefined, { scroll: false, shallow: true });
+  }
+
+  useEffect(() => {
+    setOrigin((router.query.origin as string) || location.href || '');
+  }, [origin, router.query.origin]);
+
+  return (
+    <>
+      <Head>
+        <title>Giscussions</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="w-full max-w-3xl p-2 mx-auto">
+        {!session || token ? (
+          <AuthContext.Provider value={{ token, origin }}>
+            <Giscussions repo={repo} term={term} />
+          </AuthContext.Provider>
+        ) : null}
+      </main>
+    </>
+  );
+}

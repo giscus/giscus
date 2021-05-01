6466f4202d530289aa51f9bc23afb3e3e0731469
sage <laymonage@gmail.com>
2021-05-01 08:20:28 +0700

    Dynamically import theme styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,35 @@
 import 'preact/devtools';
-import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import '../styles/themes/_base.css';
+import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/dist/client/router';
+
+function useTheme(theme: string) {
+  const themes = {
+    dark: () => {
+      import('../styles/themes/dark.css');
+      import('github-syntax-dark/lib/github-dark.css');
+    },
+    dark_dimmed: () => {
+      import('../styles/themes/dark_dimmed.css');
+      import('github-syntax-dark/lib/github-dark.css');
+    },
+    light: () => {
+      import('../styles/themes/light.css');
+      import('github-syntax-light/lib/github-light.css');
+    },
+  };
+
+  return themes[theme] || themes['light'];
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const theme = (router.query.theme as string) || 'light';
+  useTheme(theme)();
+
   return <Component {...pageProps} />;
 }
 

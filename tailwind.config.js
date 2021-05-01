1255cb0fe77703d5543ee521318384ce9a5694f5
sage <laymonage@gmail.com>
2021-05-01 11:12:18 +0700

    Update dependencies and drop @tailwindcss/jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {

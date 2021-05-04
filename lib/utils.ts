be6840822a2ac2cd53e7ce59e9a77431a391fda0
sage <laymonage@gmail.com>
2021-05-04 09:39:56 +0700

    Folow GitHub's date formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,22 @@
-import { format, formatDistanceStrict } from 'date-fns';
+import { differenceInMonths, differenceInYears, format, formatDistanceStrict } from 'date-fns';
 
 export function formatDate(dt: string) {
   return format(new Date(dt), 'LLL d, y, p O');
 }
 
 export function formatDateDistance(dt: string) {
-  return formatDistanceStrict(new Date(dt), new Date(), { addSuffix: true });
+  const inputDate = new Date(dt);
+  const now = new Date();
+
+  if (differenceInMonths(now, inputDate) >= 1) {
+    if (differenceInYears(now, inputDate) >= 1) {
+      return format(inputDate, 'LLL d, y');
+    }
+
+    return format(inputDate, 'LLL d');
+  }
+
+  return formatDistanceStrict(inputDate, now, { addSuffix: true });
 }
 
 export function isEmpty(v: unknown) {

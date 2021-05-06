4b63102a7428486f58d369614e3f93ed6d0b4b43
sage <laymonage@gmail.com>
2021-05-07 02:36:38 +0700

    Replace arrow with function for fetcher functions

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,13 +1,14 @@
 import { isEmpty } from './utils';
 
-export const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+export async function fetcher(input: RequestInfo, init?: RequestInit) {
   const res = await fetch(input, init);
   if (!res.ok) throw await res.json();
   return res.json();
-};
+}
 
-export const cleanParams = (params: Record<string, unknown>) =>
-  Object.entries(params).reduce(
+export function cleanParams(params: Record<string, unknown>) {
+  return Object.entries(params).reduce(
     (prev, [key, value]) => (!isEmpty(value) ? { ...prev, [key]: value } : prev),
     {},
   );
+}

1cee7b2868a7d7883651afa3e62df7388605e5a3
sage <laymonage@gmail.com>
2021-05-09 15:02:28 +0700

    Create ThemeContext

--- lib/context.ts
+++ lib/context.ts
@@ -8,3 +8,10 @@ export const AuthContext = createContext({
 export function getLoginUrl(origin: string) {
   return `/api/oauth/authorize?redirect_uri=${encodeURIComponent(origin)}`;
 }
+
+export const ThemeContext = createContext<{
+  theme: string;
+  setTheme?: (theme: string) => void;
+}>({
+  theme: '',
+});

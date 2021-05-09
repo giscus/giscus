6a55ce3ba24268f0ba1df6ce57953a5a57e0ff0d
sage <laymonage@gmail.com>
2021-05-09 15:01:55 +0700

    Support other methods for finding the discussion in the client script

--- client.ts
+++ client.ts
@@ -8,21 +8,37 @@ if (session) {
 }
 
 const script = document.currentScript as HTMLScriptElement;
+const attributes = script.dataset;
 const params: Record<string, string> = {};
-for (const attr of Array.from(script.attributes)) {
-  params[attr.name.replace(/^data-/, '')] = attr.value;
-}
 
 params.origin = location.href;
-params.term = location.pathname;
-params.title = document.title;
 params.session = session || localStorage.getItem('giscussions-session') || '';
+params.theme = attributes.theme;
+params.repo = attributes.repo;
 
-const iframeAttributes = {
-  class: 'giscussions-frame',
-  title: 'Comments',
-  scrolling: 'no',
-};
+switch (attributes.mapping) {
+  case 'url':
+    params.term = location.href;
+    break;
+  case 'title':
+    params.term = document.title;
+    break;
+  case 'og:title':
+    {
+      const ogtitleMeta = document.querySelector(
+        `meta[property='og:title'],meta[name='og:title']`,
+      ) as HTMLMetaElement;
+      params.term = ogtitleMeta ? ogtitleMeta.content : '';
+    }
+    break;
+  case 'specific':
+    params.term = attributes.term;
+    break;
+  case 'pathname':
+  default:
+    params.term = location.pathname;
+    break;
+}
 
 const giscussionsOrigin = script.src.match(
   /^https:\/\/giscussions\.vercel\.app|http:\/\/localhost:\d+/,
@@ -30,8 +46,13 @@ const giscussionsOrigin = script.src.match(
 const src = `${giscussionsOrigin}/widget?${new URLSearchParams(params)}`;
 
 const iframeElement = document.createElement('iframe');
-iframeElement.setAttribute('src', src);
-Object.entries(iframeAttributes).map(([key, value]) => iframeElement.setAttribute(key, value));
+const iframeAttributes = {
+  class: 'giscussions-frame',
+  title: 'Comments',
+  scrolling: 'no',
+  src,
+};
+Object.entries(iframeAttributes).forEach(([key, value]) => iframeElement.setAttribute(key, value));
 
 const existingContainer = document.querySelector('.giscussions');
 if (!existingContainer) {

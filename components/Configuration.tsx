b6943739bd4c29cf10bf61d16d704d8b58ac1681
sage <laymonage@gmail.com>
2021-05-12 14:25:30 +0700

    Add category selection to Configuration component

--- components/Configuration.tsx
+++ components/Configuration.tsx
@@ -1,7 +1,10 @@
 import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { ThemeContext } from '../lib/context';
+import { useDebounce } from '../lib/hooks';
+import { ICategory } from '../lib/types/adapter';
 import { clipboardCopy } from '../lib/utils';
 import { themeOptions } from '../lib/variables';
+import { getCategories } from '../services/giscussions/categories';
 
 const mappingOptions = [
   {
@@ -74,10 +77,15 @@ const mappingOptions = [
 
 export default function Configuration() {
   const [repository, setRepository] = useState('');
+  const [lastRepository, setLastRepository] = useState('');
+  const [repositoryId, setRepositoryId] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [categories, setCategories] = useState<ICategory[]>([]);
   const [mapping, setMapping] = useState('pathname');
   const [term, setTerm] = useState('');
   const [theme, setTheme] = useState('light');
   const [isCopied, setIsCopied] = useState(false);
+  const dRepository = useDebounce(repository);
   const scriptBox = useRef<HTMLPreElement>();
   const { setTheme: setGlobalTheme } = useContext(ThemeContext);
 
@@ -90,6 +98,14 @@ export default function Configuration() {
   useEffect(() => {
     setGlobalTheme(theme);
   }, [setGlobalTheme, theme]);
+
+  if (dRepository && dRepository !== lastRepository) {
+    getCategories(dRepository).then(({ repositoryId, categories }) => {
+      setRepositoryId(repositoryId);
+      setCategories(categories);
+      setLastRepository(dRepository);
+    });
+  }
 
   return (
     <div className="p-4 pt-0 markdown">
@@ -138,6 +154,26 @@ export default function Configuration() {
           </p>
         </div>
       </fieldset>
+
+      <h3>Discussion Category</h3>
+      <p>Choose the discussion category where new discussions will be created.</p>
+      <select
+        name="category"
+        id="category"
+        disabled={!categories.length}
+        value={categoryId}
+        onChange={(event) => setCategoryId(event.target.value)}
+        className="px-[12px] py-[5px] pr-6 min-w-[200px] border rounded-md appearance-none bg-no-repeat form-control form-select color-border-primary color-bg-primary"
+      >
+        <option value="" disabled selected>
+          {categories.length ? 'Pick a category' : 'No categories found'}
+        </option>
+        {categories.map(({ id, emoji, name }) => (
+          <option key={id} value={id}>
+            {emoji} {name}
+          </option>
+        ))}
+      </select>
 
       <h3>Page ↔️ Discussions Mapping</h3>
       <p>Choose the mapping between the embedding page and the embedded discussion.</p>
@@ -212,6 +248,12 @@ export default function Configuration() {
           <span className="pl-c1">data-repo</span>={'"'}
           <span className="pl-s">{repository || '[ENTER REPO HERE]'}</span>
           {'"\n        '}
+          <span className="pl-c1">data-repo-id</span>={'"'}
+          <span className="pl-s">{repositoryId || '[ENTER REPO ID HERE]'}</span>
+          {'"\n        '}
+          <span className="pl-c1">data-category-id</span>={'"'}
+          <span className="pl-s">{categoryId || '[ENTER CATEGORY ID HERE]'}</span>
+          {'"\n        '}
           <span className="pl-c1">data-mapping</span>={'"'}
           <span className="pl-s">{mapping}</span>
           {'"\n        '}
@@ -236,7 +278,7 @@ export default function Configuration() {
           <span className="pl-kos">{'>'}</span>
         </pre>
       </div>
-      <div className="w-full text-right">
+      <div className="w-full mb-4 text-right">
         <span className={`mr-2${!isCopied ? ' hidden' : ''}`}>✔️ Copied!</span>
         <button
           className="px-4 py-[5px] ml-1 border rounded-md btn btn-primary"

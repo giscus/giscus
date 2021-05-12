2252aece53eaf5d14c51a3e3e7ebeb0f5cb81b84
sage <laymonage@gmail.com>
2021-05-12 13:52:37 +0700

    Create getDiscussionCategories service function

---
+++ services/github/getDiscussionCategories.ts
@@ -0,0 +1,59 @@
+import { GDiscussionCategory, GError } from '../../lib/types/github';
+import { getReadAccessToken } from './getReadAccessToken';
+
+const GITHUB_API_URL = 'https://api.github.com/graphql';
+
+const GET_DISCUSSION_CATEGORIES_QUERY = `
+  query($query: String!) {
+    search(type: REPOSITORY query: $query first:1) {
+      nodes {
+        ... on Repository {
+          id
+          discussionCategories(first: 100) {
+            nodes {
+              id
+              name
+              emojiHTML
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+export interface GetDiscussionCategoriesParams {
+  repo: string;
+}
+
+export interface GetDiscussionCategoriesResponse {
+  data: {
+    search: {
+      nodes: Array<{
+        id: string;
+        discussionCategories: {
+          nodes: GDiscussionCategory[];
+        };
+      }>;
+    };
+  };
+}
+
+export async function getDiscussionCategories(
+  params: GetDiscussionCategoriesParams,
+  token?: string,
+): Promise<GetDiscussionCategoriesResponse | GError> {
+  const query = `repo:${params.repo}`;
+
+  return fetch(GITHUB_API_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token || (await getReadAccessToken())}`,
+      'GraphQL-Features': 'discussions_api',
+    },
+
+    body: JSON.stringify({
+      query: GET_DISCUSSION_CATEGORIES_QUERY,
+      variables: { query },
+    }),
+  }).then((r) => r.json());
+}

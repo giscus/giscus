297dabc209ed02f906d45b5cc4d1d7d91b5e400c
sage <laymonage@gmail.com>
2021-05-12 13:56:08 +0700

    Create /api/categories API route

---
+++ pages/api/categories.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ICategories } from '../../lib/types/adapter';
+import { getDiscussionCategories } from '../../services/github/getDiscussionCategories';
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ICategories | { error: string }>,
+) => {
+  const token = req.headers.authorization?.split('Bearer ')[1];
+
+  const params = { repo: req.query.repo as string };
+  const result = { repositoryId: '', categories: [] };
+
+  const response = await getDiscussionCategories(params, token);
+
+  if ('message' in response) {
+    res.status(500).json({ error: response.message });
+    return;
+  }
+
+  const {
+    data: {
+      search: { nodes: repositories },
+    },
+  } = response;
+
+  const repository = repositories[0];
+  if (!repository) {
+    res.status(404).json(result);
+    return;
+  }
+
+  const {
+    id: repositoryId,
+    discussionCategories: { nodes },
+  } = repository;
+  const categories = nodes.map(({ emojiHTML, ...rest }) => ({
+    emoji: emojiHTML?.match(/">(.*?)<\/g-emoji/)[1] || '',
+    ...rest,
+  }));
+
+  res.status(200).json({ repositoryId, categories });
+};

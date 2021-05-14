db36215dabbb7569951c71298bd2c0cd3d18ef17
sage <laymonage@gmail.com>
2021-05-15 06:12:13 +0700

    Add meta and link tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,13 @@ import { useEffect, useState } from 'react';
 import { ThemeContext } from '../lib/context';
 import { getTheme } from '../lib/utils';
 
+const meta = {
+  title: 'giscussions',
+  description: 'A comments widget built on GitHub Discussions.',
+  image:
+    'https://opengraph.githubassets.com/5500584364ff6fde70d120e51f28f33ffe97d8f4661517bba2ab9515d0765857/laymonage/giscussions',
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [theme, setTheme] = useState(router.query.theme as string);
@@ -24,9 +31,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeContext.Provider value={{ theme: getTheme(theme), setTheme }}>
       <Head>
-        <title>Giscussions</title>
+        <title>{meta.title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://api.github.com" />
+        <link rel="preconnect" href="https://avatars3.githubusercontent.com" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@laymonage" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
         <link rel="stylesheet" crossOrigin="anonymous" href={`/themes/${getTheme(theme)}.css`} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </ThemeContext.Provider>

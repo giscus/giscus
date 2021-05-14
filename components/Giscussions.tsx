3cf524a530420bfd27c5223f2cd7f4435ea086b3
sage <laymonage@gmail.com>
2021-05-14 07:58:10 +0700

    Always call onError if error

--- components/Giscussions.tsx
+++ components/Giscussions.tsx
@@ -91,7 +91,7 @@ export default function Giscussions({
   const error = frontError || backError;
   const shouldShowReplyCount = !error && !isNotFound && !isLoading;
 
-  if (error && !(error?.message || '').includes('not installed') && onError) onError();
+  if (error && onError) onError();
 
   return (
     <div className="w-full color-text-primary">

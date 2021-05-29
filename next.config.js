c32cfeae294f36cfb6d0cd8e4592b2b105f29c70
sage <laymonage@gmail.com>
2021-05-29 14:30:23 +0700

    Opt-out of FLoC

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 const withPreact = require('next-plugin-preact');
 
-module.exports = withPreact({});
+module.exports = withPreact({
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'interest-cohort=()',
+          },
+        ],
+      },
+    ];
+  },
+});

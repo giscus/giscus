d1865f532ae1d1aba84f0f7a59d5ce86377ed3c8
sage <laymonage@gmail.com>
2021-06-01 03:04:35 +0700

    Create updateDiscussionReaction function

--- lib/reactions.ts
+++ lib/reactions.ts
@@ -1,4 +1,4 @@
-import { IComment, IReply } from './types/adapter';
+import { IComment, IGiscussion, IReactionGroups, IReply } from './types/adapter';
 
 export const Reactions = {
   THUMBS_UP: { name: '+1', emoji: '👍' },
@@ -13,20 +13,34 @@ export const Reactions = {
 
 export type Reactions = keyof typeof Reactions;
 
+function updateReactionGroups(reactionGroups: IReactionGroups, reaction: Reactions) {
+  return {
+    ...reactionGroups,
+    [reaction]: {
+      count: reactionGroups[reaction].viewerHasReacted
+        ? reactionGroups[reaction].count - 1
+        : reactionGroups[reaction].count + 1,
+      viewerHasReacted: !reactionGroups[reaction].viewerHasReacted,
+    },
+  };
+}
+
+export function updateDiscussionReaction(page: IGiscussion, reaction: Reactions) {
+  return {
+    ...page,
+    discussion: {
+      ...page.discussion,
+      reactions: updateReactionGroups(page.discussion.reactions, reaction),
+    },
+  } as IGiscussion;
+}
+
 export function updateCommentReaction<T extends IComment | IReply = IComment>(
   comment: T,
   reaction: Reactions,
 ) {
   return {
     ...comment,
-    reactions: {
-      ...comment.reactions,
-      [reaction]: {
-        count: comment.reactions[reaction].viewerHasReacted
-          ? comment.reactions[reaction].count - 1
-          : comment.reactions[reaction].count + 1,
-        viewerHasReacted: !comment.reactions[reaction].viewerHasReacted,
-      },
-    },
+    reactions: updateReactionGroups(comment.reactions, reaction),
   } as T;
 }

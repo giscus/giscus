4e947652e379cfc2c4e8fcfb18a6a9f2b7d2a1d3
sage <laymonage@gmail.com>
2021-06-01 03:04:52 +0700

    Add breakpoints using min-device-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      screens: {
+        // To use device width instead of min-width.
+        // Might be useful because min-width will take the iframe's width instead.
+        dsm: { raw: '(min-device-width: 640px)' },
+        dmd: { raw: '(min-device-width: 768px)' },
+        dlg: { raw: '(min-device-width: 1024px)' },
+        dxl: { raw: '(min-device-width: 1280px)' },
+        d2xl: { raw: '(min-device-width: 1536px)' },
+      },
       fontFamily: {
         sans: [
           '-apple-system',

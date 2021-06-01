d443841c5333d964cd74f5dd34416575e450af2c
sage <laymonage@gmail.com>
2021-06-01 14:07:18 +0700

    Use IError in API routes

--- pages/api/discussions/categories.ts
+++ pages/api/discussions/categories.ts
@@ -1,12 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { ICategories } from '../../../lib/types/adapter';
+import { ICategories, IError } from '../../../lib/types/adapter';
 import { getAppAccessToken } from '../../../services/github/getAppAccessToken';
 import { getDiscussionCategories } from '../../../services/github/getDiscussionCategories';
 
-export default async (
-  req: NextApiRequest,
-  res: NextApiResponse<ICategories | { error: string }>,
-) => {
+export default async (req: NextApiRequest, res: NextApiResponse<ICategories | IError>) => {
   const params = { repo: req.query.repo as string };
   const result = { repositoryId: '', categories: [] };
 

826760bbb51dc01596c36d57cf9666a69654d84f
sage <laymonage@gmail.com>
2021-06-03 23:28:47 +0700

    Put all GitHub API URL variables in config

--- services/config.ts
+++ services/config.ts
@@ -1,1 +1,13 @@
-export const GITHUB_GRAPHQL_API_URL = 'https://api.github.com/graphql';
+const GITHUB_API_HOST = 'https://api.github.com';
+
+export const GITHUB_GRAPHQL_API_URL = `${GITHUB_API_HOST}/graphql`;
+
+export const GITHUB_MARKDOWN_API_URL = `${GITHUB_API_HOST}/markdown`;
+
+export const GITHUB_INSTALLATIONS_URL = `${GITHUB_API_HOST}/app/installations`;
+
+export const GITHUB_REPO_INSTALLATION_URL = (repoWithOwner: string) =>
+  `${GITHUB_API_HOST}/repos/${repoWithOwner}/installation`;
+
+export const GITHUB_ACCESS_TOKEN_URL = (id: string) =>
+  `${GITHUB_INSTALLATIONS_URL}/${id}/access_tokens`;

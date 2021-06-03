e0da9b59f548b28c5b66e26bca3d44f91ab6aeaa
sage <laymonage@gmail.com>
2021-06-03 23:20:55 +0700

    Add notes about stub webhook API route

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// This is not used at all, but it is a requirement for the app
+// to be listed on the marketplace.
+
 export default async (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ success: true });
 };

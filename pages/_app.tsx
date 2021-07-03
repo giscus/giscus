258773313a445f9ea4459433a43bb95ce5607d46
sage <laymonage@gmail.com>
2021-07-03 11:31:43 +0700

    Remove unnecessary theme in useEffect dependency list

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
     const listener = () => rerender({});
     mediaQuery.addEventListener('change', listener);
     return () => mediaQuery.removeEventListener('change', listener);
-  }, [theme]);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme: getTheme(theme), setTheme }}>

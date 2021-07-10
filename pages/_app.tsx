ed933326a1630f0c8314f722a8947fa000ed8ea6
sage <laymonage@gmail.com>
2021-07-10 18:04:16 +0700

    Use custom document to include initial CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,22 +25,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeContext.Provider value={{ theme: resolvedTheme, setTheme }}>
       <Head>
         <title>{meta.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://api.github.com" />
-        <link rel="preconnect" href="https://avatars3.githubusercontent.com" />
         <meta name="description" content={meta.description} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@laymonage" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </ThemeContext.Provider>

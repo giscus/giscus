d393598971cbd97143bcc433f3865ddbfa0989e1
sage <laymonage@gmail.com>
2021-07-11 09:14:35 +0700

    Create types for window messages

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -2,6 +2,7 @@ import { useCallback, useContext, useEffect, useState } from 'react';
 import Giscus from '../components/Giscus';
 import { AuthContext, ConfigContext, getLoginUrl } from '../lib/context';
 import { emitData } from '../lib/messages';
+import { IErrorMessage } from '../lib/types/giscus';
 import { createDiscussion } from '../services/giscus/createDiscussion';
 import { getToken } from '../services/giscus/token';
 
@@ -27,7 +28,7 @@ export default function Widget({ origin, session, repoId, categoryId, descriptio
 
   const handleError = useCallback(
     (message: string) => {
-      emitData({ error: message }, origin);
+      emitData<IErrorMessage>({ error: message }, origin);
     },
     [origin],
   );

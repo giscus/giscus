eeae5ea2a7e16983df897ce3f1b533726fc3b5fd
sage <laymonage@gmail.com>
2021-07-11 11:53:21 +0700

    Create sendData function to send data from parent to giscus

--- lib/messages.ts
+++ lib/messages.ts
@@ -3,3 +3,9 @@ import { IMessage } from './types/giscus';
 export function emitData<T>(data: T, origin: string) {
   window.parent.postMessage({ giscus: data } as IMessage<T>, origin);
 }
+
+export function sendData<T>(data: T, origin: string) {
+  const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+  if (!iframe) return;
+  iframe.contentWindow.postMessage({ giscus: data } as IMessage<T>, origin);
+}

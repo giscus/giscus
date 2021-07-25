274931006c9fe6e1da3803f634d06df640cffecf
sage <laymonage@gmail.com>
2021-07-25 10:55:41 +0700

    Add security headers

--- next.config.js
+++ next.config.js
@@ -3,18 +3,44 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `frame-ancestors 'self';`,
+  }
+];
+
 module.exports = withBundleAnalyzer(
   withPreact({
     async headers() {
       return [
         {
           source: '/(.*)',
-          headers: [
-            {
-              key: 'Permissions-Policy',
-              value: 'interest-cohort=()',
-            },
-          ],
+          headers: securityHeaders,
         },
       ];
     },

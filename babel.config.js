417fb8a8ecde24b8071006fc126823d919e8a827
sage <laymonage@gmail.com>
2021-07-25 14:35:09 +0700

    Upgrade dependencies

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = {
       'next/babel',
       {
         'preset-env': {
-          debug: true,
           targets: {
             browsers: '>0.3%, not ie 11, not dead, not op_mini all',
           },

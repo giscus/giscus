b48df1163183dbeb26ffcccf9d7e285245d4b8be
sage <laymonage@gmail.com>
2021-08-13 19:00:55 +0700

    Use next/script to load iFrameResizer

--- pages/widget.tsx
+++ pages/widget.tsx
@@ -1,5 +1,6 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
+import Script from 'next/script';
 import { ContextType, useContext, useEffect, useState } from 'react';
 import Widget from '../components/Widget';
 import { assertOrigin } from '../lib/config';
@@ -134,11 +135,11 @@ export default function WidgetPage({
         </ConfigContext.Provider>
       </main>
 
-      <script
+      <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.1/iframeResizer.contentWindow.min.js"
         integrity="sha512-qw2bX9KUhi7HLuUloyRsvxRlWJvj0u0JWVegc5tf7qsw47T0pwXZIk1Kyc0utTH3NlrpHtLa4HYTVUyHBr9Ufg=="
         crossOrigin="anonymous"
-      ></script>
+      />
     </>
   );
 }

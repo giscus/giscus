da742ea43f21270bb4dc50410c8d5ffc9590770f
sage <laymonage@gmail.com>
2021-11-06 16:35:05 +0700

    Replace custom SVG with MarkGithubIcon

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -1,4 +1,4 @@
-import { MarkdownIcon } from '@primer/octicons-react';
+import { MarkdownIcon, MarkGithubIcon } from '@primer/octicons-react';
 import { ChangeEvent, useCallback, useContext, useEffect, useState } from 'react';
 import { adaptComment, adaptReply, handleCommentClick, processCommentBody } from '../lib/adapter';
 import { AuthContext } from '../lib/context';
@@ -209,20 +209,7 @@ export default function CommentBox({
               target="_top"
               href={loginUrl}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 11.493 11.209"
-                className="mr-2"
-              >
-                <path
-                  fill="#fff"
-                  fillRule="evenodd"
-                  d="M5.746 0A5.747 5.747 0 003.93 11.2c.287.052.392-.125.392-.278 0-.136-.005-.497-.008-.977-1.598.347-1.935-.77-1.935-.77-.262-.664-.639-.841-.639-.841-.521-.356.04-.35.04-.35.577.041.88.593.88.593.513.878 1.345.625 1.673.477.052-.37.2-.624.364-.768-1.276-.145-2.617-.638-2.617-2.84 0-.627.224-1.14.591-1.542-.059-.145-.256-.73.057-1.52 0 0 .482-.155 1.58.589.458-.128.95-.192 1.438-.194.489.002.98.066 1.44.194 1.096-.744 1.578-.59 1.578-.59.313.791.116 1.376.057 1.521.369.402.59.915.59 1.542 0 2.208-1.343 2.694-2.623 2.836.206.177.39.528.39 1.064 0 .768-.007 1.388-.007 1.576 0 .154.104.333.395.277A5.749 5.749 0 005.746 0"
-                />
-              </svg>{' '}
-              {t('signInWithGitHub')}
+              <MarkGithubIcon className="mr-2" fill="currentColor" /> {t('signInWithGitHub')}
             </a>
           )}
         </div>

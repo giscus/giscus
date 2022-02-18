09e4dbd24aade8a933dbcd1ab9a13ef36fd68b51
sage <laymonage@gmail.com>
2022-02-18 10:00:19 +0700

    Upgrade Next.js to 12.1.0

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      'next/babel',
-      {
-        'preset-env': {
-          targets: {
-            browsers: '>0.3%, not ie 11, not dead, not op_mini all',
-          },
-        },
-      },
-    ],
-  ],
+  presets: ['next/babel'],
 };

076ce32d4100d5061778dacd96fedc1d4087d4ae
Sage Abdullah <me@laymonage.com>
2022-04-09 14:16:42 +0700

    Ignore max-line-length rule for @apply rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   rules: {
     'selector-class-pattern': '[A-z]+(-[a-z]+)*',
+    'max-line-length': [
+      120,
+      {
+        ignorePattern: '/@apply\\s+/',
+      },
+    ],
   },
 };

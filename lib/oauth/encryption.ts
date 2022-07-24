giscus/giscus

commit a0575f9a73d9fabc3bfd44a38ca00ae5d10effee

Create digestMessage function

lib/utils.ts
@@ -1,4 +1,5 @@
 import { AvailableTheme, availableThemes, Theme } from './variables';
+import { webcrypto } from 'crypto';
 
 function isAvailableTheme(theme: Theme): theme is AvailableTheme {
   return availableThemes.includes(theme as AvailableTheme);
@@ -63,4 +64,15 @@ export function resizeTextArea(textarea: HTMLTextAreaElement) {
   textarea.style.height = `0px`;
   const height = textarea.scrollHeight <= maxHeight ? textarea.scrollHeight : maxHeight;
   textarea.style.height = `${height}px`;
+}
+
+export async function digestMessage(message: string, algorithm: AlgorithmIdentifier = 'SHA-1') {
+  const crypto = webcrypto as unknown as Crypto;
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+  const msgUint8 = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }

commit 7bb1edc11b549f91a82a51e1eaee72e19ca239f3

Fix node webcrypto module from being included in client bundle
Regression in a0575f9a73d9fabc3bfd44a38ca00ae5d10effee

lib/oauth/encryption.ts
@@ -1,7 +1,4 @@
-import { webcrypto } from 'crypto';
-
-// @types/node only provides stub types for webcrypto, so use the types from DOM.
-const crypto = webcrypto as unknown as Crypto;
+import { webcrypto } from '../utils';
 
 /**
  * Encrypts plaintext using AES-GCM with supplied password, for decryption with aesGcmDecrypt().
@@ -17,16 +14,16 @@ const crypto = webcrypto as unknown as Crypto;
  */
 export async function aesGcmEncrypt(plaintext: string, password: string): Promise<string> {
   const pwUtf8 = new TextEncoder().encode(password); // encode password as UTF-8
-  const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8); // hash the password
+  const pwHash = await webcrypto.subtle.digest('SHA-256', pwUtf8); // hash the password
 
-  const iv = crypto.getRandomValues(new Uint8Array(12)); // get 96-bit random iv
+  const iv = webcrypto.getRandomValues(new Uint8Array(12)); // get 96-bit random iv
 
   const alg = { name: 'AES-GCM', iv: iv }; // specify algorithm to use
 
-  const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['encrypt']); // generate key from pw
+  const key = await webcrypto.subtle.importKey('raw', pwHash, alg, false, ['encrypt']); // generate key from pw
 
   const ptUint8 = new TextEncoder().encode(plaintext); // encode plaintext as UTF-8
-  const ctBuffer = await crypto.subtle.encrypt(alg, key, ptUint8); // encrypt plaintext using key
+  const ctBuffer = await webcrypto.subtle.encrypt(alg, key, ptUint8); // encrypt plaintext using key
 
   const ctArray = Array.from(new Uint8Array(ctBuffer)); // ciphertext as byte array
   const ctStr = ctArray.map((byte) => String.fromCharCode(byte)).join(''); // ciphertext as string
@@ -53,7 +50,7 @@ export async function aesGcmEncrypt(plaintext: string, password: string): Promis
  */
 export async function aesGcmDecrypt(ciphertext: string, password: string): Promise<string> {
   const pwUtf8 = new TextEncoder().encode(password); // encode password as UTF-8
-  const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8); // hash the password
+  const pwHash = await webcrypto.subtle.digest('SHA-256', pwUtf8); // hash the password
 
   const iv = ciphertext
     .slice(0, 24)
@@ -62,13 +59,13 @@ export async function aesGcmDecrypt(ciphertext: string, password: string): Promi
 
   const alg = { name: 'AES-GCM', iv: new Uint8Array(iv) }; // specify algorithm to use
 
-  const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['decrypt']); // use pw to generate key
+  const key = await webcrypto.subtle.importKey('raw', pwHash, alg, false, ['decrypt']); // use pw to generate key
 
   const ctStr = Buffer.from(ciphertext.slice(24), 'base64').toString('binary'); // decode base64 ciphertext
   const ctUint8 = new Uint8Array(ctStr.match(/[\s\S]/g).map((ch) => ch.charCodeAt(0))); // ciphertext as Uint8Array
   // note: why doesn't ctUint8 = new TextEncoder().encode(ctStr) work?
 
-  const plainBuffer = await crypto.subtle.decrypt(alg, key, ctUint8); // decrypt ciphertext using key
+  const plainBuffer = await webcrypto.subtle.decrypt(alg, key, ctUint8); // decrypt ciphertext using key
   const plaintext = new TextDecoder().decode(plainBuffer); // decode password from UTF-8
 
   return plaintext; // return the plaintext

lib/utils.ts
@@ -1,5 +1,14 @@
 import { AvailableTheme, availableThemes, Theme } from './variables';
-import { webcrypto } from 'crypto';
+
+export let webcrypto: Crypto;
+
+if (typeof window === 'undefined') {
+  import('crypto').then((module) => {
+    webcrypto = module.webcrypto;
+  });
+} else {
+  webcrypto = window.crypto;
+}
 
 function isAvailableTheme(theme: Theme): theme is AvailableTheme {
   return availableThemes.includes(theme as AvailableTheme);
@@ -67,11 +76,9 @@ export function resizeTextArea(textarea: HTMLTextAreaElement) {
 }
 
 export async function digestMessage(message: string, algorithm: AlgorithmIdentifier = 'SHA-1') {
-  const crypto = webcrypto as unknown as Crypto;
-
   // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
   const msgUint8 = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8);
+  const hashBuffer = await webcrypto.subtle.digest(algorithm, msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
   return hashHex;
af945953a8ef61c903e0095e01cdf60223d928db
Sage Abdullah <me@laymonage.com>
2023-06-24 22:08:41 +0100

    Update stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   rules: {
     'selector-class-pattern': '[A-z]+(-[a-z]+)*',
+    'color-function-notation': ['modern', { ignore: ['with-var-inside'] }],
     'max-line-length': [
       120,
       {
